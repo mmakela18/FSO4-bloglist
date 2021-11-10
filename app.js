5bc4a1e2e0309f502974aad27ebb05b3eb83bc03
Mikko Mäkelä <mmakela18@student.oulu.fi>
2021-11-10 22:25:30 +0200

    differentiated app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const express = require('express')
+const blogsRouter = require('./controllers/blogs.js')
+const logger = require('./utils/logger')
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+logger.info("connected to mongodb")
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', blogsRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs.js')
-
-app.use('/api', blogsRouter)
-
-mongoose.connect(config.MONGODB_URI)
-logger.info("connected to mongodb")
-app.use(cors())
-app.use(express.json())
-
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
