7d631da1f57b8b4547eca929daff61a8b950813b
Mikko Mäkelä <mmakela18@student.oulu.fi>
2021-11-12 14:37:24 +0200

    4.4: tests work

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
   return 1
 }
 
+// Return number of likes from an array of blog-objects
+const totalLikes = (blogs) => {
+    // Check special cases
+    if (blogs.length === 0) return 0
+    if (blogs.length === 1) return blogs[0].likes
+    // Reducer must return object with the wanted field
+    const reducer = (prev, curr) => {
+        return {
+          likes: prev.likes + curr.likes
+        }
+    }
+    // Only return likes from this reduced object
+    return blogs.reduce(reducer).likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
