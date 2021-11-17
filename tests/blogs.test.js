e0f26da4fbc0f3b90e182ee9d568573e158098b2
Mikko Mäkelä <mmakela18@student.oulu.fi>
2021-11-17 19:32:56 +0200

    4.10 POST-test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -30,8 +30,29 @@ test('id defined as "id"', async () => {
   // take the first entry
   const entry = res.body[0]
   expect(entry.id).toBeDefined()
+})
+
+test('can POST a blog entry', async () => {
+  // single new entry
+  const toPost = {
+    title: "Creation",
+    author: "God",
+    url: "everywhere",
+    likes: 1,
+    __v: 0
+  }
+  // POST
+  postRes = await api.post('/api/blogs')
+    .send(toPost)
+    .expect(201)
+  // see that length actually increased
+  const res = await api.get('/api/blogs')
+  expect(res.body).toHaveLength(blogs.length + 1)
+  // see that the entry actually matches what was sent
+  expect(res.body).toContainEqual(postRes.body)
 
 })
+
 
 afterAll( () => {
   mongoose.connection.close()
